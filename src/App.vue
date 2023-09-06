4b142746e37b9d6a754ddd38b0a9f3c866275ef2
filipj169 <jamrichfilip16@gmail.com>
2023-09-06 15:47:03 +0200

    first push

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-gray-100">
+    <div class="container mx-auto pt-4">
+      <Navigation />
+      <p>HomePage</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Navigation from "./components/Navigation/Navigation.vue";
+
+</script>
---
+++ src/components/Navigation/Navigation.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex justify-between items-center bg-white rounded">
+    <a><img src="../../assets/images/logo-devlinks-large.svg" alt="devlinks"></a>
+    <div class="flex gap-5">
+      <div class="flex gap-1">
+        <img src="../../assets/icons/icon-link.svg" alt="Link icon">
+        <p>Links</p>
+      </div>
+      <div class="flex gap-1">
+        <img src="../../assets/icons/icon-profile-details-header.svg" alt="My profile icon">
+        <p class="whitespace-nowrap">Profile Details</p>
+      </div>
+    </div>
+    <button>Preview</button>
+  </div>
+</template>
+
+<script setup>
+</script>
